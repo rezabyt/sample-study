3f97cd5426a9457d0c167334ffab1654ee6d8ced
rezabyt <bayat.reza.br@gmail.com>
2024-04-15 21:55:14 -0400

    refactor the code to save checkpoints more frequently at the beginning of the training

--- trainer.py
+++ trainer.py
@@ -45,6 +45,12 @@ class NT:
         for batch_idx, batch in enumerate(self.train_loader):
             loss = self.update(batch=batch, epoch=epoch, batch_idx=batch_idx, loader_length=len(self.train_loader.dataset))
             epoch_loss += loss.item() / len(self.train_loader)
+            
+            base_epoch = self.args.epochs // 10
+            num_batches = len(self.train_loader)
+            if batch_idx % ((epoch + 1) * (num_batches // base_epoch)) == 0 and batch_idx != 0:
+                self.eval(epoch=epoch, step=batch_idx)
+                self.save_model(epoch=epoch, step=batch_idx)
 
         wandb.log({'Train Loss': epoch_loss, 'epoch': epoch})
 
@@ -53,7 +59,7 @@ class NT:
 
         return epoch_loss
 
-    def evaluate(self, loader, adversary, epoch):
+    def evaluate(self, loader, epoch, step, adversary):
         self.model.eval()
 
         loss = 0
@@ -76,32 +82,35 @@ class NT:
             loss += self.get_loss(y_hat, y).item()
             pred = y_hat.max(1, keepdim=True)[1]
             correct += pred.eq(y.view_as(pred)).sum().item()
+        
+        self.model.train()
 
         loss /= len(loader)
         accuracy = correct / len(loader.dataset)
 
-        mode = 'ADV' if adversary else 'CLN'
-        print(f'{loader.name.upper()} {mode}: Loss: {loss:.5f}, Accuracy: {100. * accuracy:2.2f}%')
+        if step == 'end':
+            mode = 'ADV' if adversary else 'CLN'
+            print(f'{loader.name.upper()} {mode}: Loss: {loss:.5f}, Accuracy: {100. * accuracy:2.2f}%')
 
-        loss_key_name = f'{loader.name.upper()} {mode} Loss'
-        wandb.log({loss_key_name: loss, 'epoch': epoch})
+            loss_key_name = f'{loader.name.upper()} {mode} Loss'
+            wandb.log({loss_key_name: loss, 'epoch': epoch})
 
-        acc_key_name = f'{loader.name.upper()} {mode} Accuracy'
-        wandb.log({acc_key_name: accuracy, 'epoch': epoch})
+            acc_key_name = f'{loader.name.upper()} {mode} Accuracy'
+            wandb.log({acc_key_name: accuracy, 'epoch': epoch})
 
-        return {'epoch': epoch, 'loss': loss, 'accuracy': accuracy, 'y_hats': np.array(y_hats), 'ys': np.array(ys)}
+        return {'epoch': epoch, 'step': step, 'loss': loss, 'accuracy': accuracy, 'y_hats': np.array(y_hats), 'ys': np.array(ys)}
     
-    def eval(self, epoch):
+    def eval(self, epoch, step='end'):
         # Evaluation on the train clean examples
-        self.evaluate(loader=self.train_loader, adversary=None, epoch=epoch)
+        self.evaluate(loader=self.train_loader, epoch=epoch, step=step, adversary=None)
 
         # Evaluation on test clean examples and save the stats
-        test_clean_stats = self.evaluate(loader=self.test_loader, adversary=None, epoch=epoch)
-        self.save_data_stats(self.exp_directory, test_clean_stats, epoch, 'test_clean')
+        test_clean_stats = self.evaluate(loader=self.test_loader, epoch=epoch, step=step, adversary=None)
+        self.save_data_stats(test_clean_stats, epoch, step, 'test_clean')
         
 
-    def save_data_stats(self, exp_directory, stats, epoch, mode):
-        stat_path = os.path.join(exp_directory, 'stats', f'{mode}_epoch_{epoch}.pkl')
+    def save_data_stats(self, stats, epoch, step, mode):
+        stat_path = os.path.join(self.exp_directory, 'stats', f'{mode}_epoch_{epoch}_step_{step}.pkl')
 
         # Save the stats to a file
         with open(stat_path, 'wb') as file:
@@ -150,14 +159,14 @@ class AT(NT):
         batch = (X_adv, y)
         return super().update(batch, epoch, batch_idx, loader_length)
 
-    def eval(self, epoch):
+    def eval(self, epoch, step='end'):
         # Evaluation on the train clean examples
-        self.evaluate(loader=self.train_loader, adversary=None, epoch=epoch)
+        self.evaluate(loader=self.train_loader, epoch=epoch, step=step, adversary=None)
 
         # Evaluation on test clean examples and save the stats
-        test_clean_stats = self.evaluate(loader=self.test_loader, adversary=None, epoch=epoch)
-        self.save_data_stats(self.exp_directory, test_clean_stats, epoch, 'test_clean')
+        test_clean_stats = self.evaluate(loader=self.test_loader, epoch=epoch, step=step, adversary=None)
+        self.save_data_stats(test_clean_stats, epoch, step, 'test_clean')
 
         # Evaluation on test adversarial examples and save the stats
-        test_adv_stats = self.evaluate(loader=self.test_loader, adversary=self.adversary, epoch=epoch)
-        self.save_data_stats(self.exp_directory, test_adv_stats, epoch, 'test_adv')
+        test_adv_stats = self.evaluate(loader=self.test_loader, epoch=epoch, step=step, adversary=self.adversary)
+        self.save_data_stats(test_adv_stats, epoch, step, 'test_adv')
